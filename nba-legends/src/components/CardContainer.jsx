a8989a1d20af4d88c75fb71bec7ab3320dfb22f1
axel-ac <abdullahceylan.axel@gmail.com>
2023-03-08 10:24:06 +0300

    PlayerCard

--- nba-legends/src/components/CardContainer.jsx
+++ nba-legends/src/components/CardContainer.jsx
@@ -1,1 +1,21 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { data } from "../helpers/data";
+import PlayerCard from "./PlayerCard";
 
+const CardContainer = () => {
+  return (
+    <Container className="card-container rounded-4 my-4 p-3">
+      <Row>
+        {data.map((player, index) => (
+          <Col key={index}>
+            <PlayerCard {...player} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default CardContainer;
--- nba-legends/src/components/PlayerCard.jsx
+++ nba-legends/src/components/PlayerCard.jsx
@@ -1,1 +1,15 @@
+import Card from "react-bootstrap/Card";
 
+const PlayerCard = ({name, img, statistics}) => {
+  // const {name, img, statistics}
+  return (
+    <Card>
+      <Card.Img variant="top" src={img} />
+      <Card.Footer>
+        <Card.Title>{name}</Card.Title>
+      </Card.Footer>
+    </Card>
+  );
+};
+
+export default PlayerCard;
